c0b07db57d4384a775d9a5170755c6bb19ac3bbb
lai2-01 <lai2-01@itismeucci.com>
2021-11-29 11:11:49 +0100

    Commit

--- src/main/java/com/itismeucci/bencini/compito/Server.java
+++ src/main/java/com/itismeucci/bencini/compito/Server.java
@@ -9,14 +9,14 @@ public class Server {
     String val1 = null;
     String val2 = null;
     String segno = null;
-
+    String cheFai = null;
     String Risultato = null;
     BufferedReader inDalClient;
     DataOutputStream outVersoClient;
     Calcolatrice calcolatrice = null;
 
-    public Socket attendi(){
-        try{
+    public Socket attendi() {
+        try {
             System.out.println("S -> Server partito in esecuzione");
 
             server = new ServerSocket(4335);
@@ -27,7 +27,7 @@ public class Server {
 
             inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
             outVersoClient = new DataOutputStream(client.getOutputStream());
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Errore durante l'istanza del server!");
             System.exit(1);
@@ -37,17 +37,33 @@ public class Server {
 
     public void comunica(){
         try {
-            System.out.println("Benvenuto");
-            outVersoClient.writeBytes("S -> Inserisci il primo valore\n");
-            val1 = inDalClient.readLine();
-            outVersoClient.writeBytes("S -> Inserisci il secondo valore\n");
-            val2 = inDalClient.readLine();
-            outVersoClient.writeBytes("S -> Inserisci il segno (+,-,*,/)\n");
-            segno = inDalClient.readLine();
-            calcolatrice = new Calcolatrice(val1, val2, segno);
-            outVersoClient.writeBytes("S -> Ti servo ancora?\n");
+            System.out.println("Benvenuto io sono un server calcolatrice!\n");
+            do{
+                outVersoClient.writeBytes("S -> Inserisci il primo valore\n");
+                val1 = inDalClient.readLine();
+                outVersoClient.writeBytes("S -> Inserisci il secondo valore\n");
+                val2 = inDalClient.readLine();
+                outVersoClient.writeBytes("S -> Inserisci il segno (+,-,*,/)\n");
+                segno = inDalClient.readLine();
+                calcolatrice = new Calcolatrice(val1, val2, segno);
+                outVersoClient.writeBytes("S -> Ti servo ancora? Y/N\n");
+                cheFai = inDalClient.readLine();
+            }while(cheFai.equalsIgnoreCase("Y"));
+
+
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    public void close(){
+        try {
+            inDalClient.close();
+            outVersoClient.close();
+            client.close();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 }
